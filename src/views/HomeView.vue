8dce84592f5db4c3283ef00cad040a6fce172840
venkatesh <venkatesh.vellingiri@dotdashmdp.com>
2024-02-01 16:38:18 +0530

    Created a simple vue app and modified the fav icon and pasted the learn page image

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+<div class="learn">
+  <img src="../assets/learn.png">
+</div>
+</template>
+
+<style>
+.learn img {
+  width:100%;
+  height: 100%;
+}
+</style>
