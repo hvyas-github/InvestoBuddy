34a0b22b4bbcf0349086160819dbe80b295a086f
hvyas-github <hvyas.github@gmail.com>
2024-02-02 13:44:29 +0530

    Investobuddy hard-coded demo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script>
 import { VueBotUI } from "vue-bot-ui";
+import BotIcon from '../assets/bot-logo.webp'
 export default {
   components: {
     VueBotUI,
@@ -7,6 +8,11 @@ export default {
   data() {
     return {
       botTyping: false,
+      botOptions: {
+        botTitle: 'InvestoBuddy',
+        colorScheme: '#5b7ce8',
+        botAvatarImg: BotIcon
+      },
       messages: [
         // {
         //   agent: 'user', // Required. 'bot' or 'user'
@@ -72,12 +78,27 @@ export default {
        if(inputValue ===  "hello") {
           console.log("matched");
           reply = {
-            text: "Welcome to Simulator Learn section. How can I help you ?",
+            text: "Hi I am InvestoBuddy. How can I help you ?",
             agent: "bot",
             type: "text",
           };
        }
-       if(inputValue === "learn options trading") {
+       if(inputValue === "What is ETF?") {
+        console.log('matched')
+          reply = {
+            type: "button",
+            text: "An ETF, or Exchange-Traded Fund, is a type of investment fund and exchange-traded product, with shares that are tradeable on a stock exchange. To know more visit: ",
+            agent: 'bot',
+            options: [
+              {
+                text: "Click to learn about ETF",
+                value: "https://www.investopedia.com/terms/e/etf.asp",
+                action: "url",
+              },
+            ]
+          };
+      }
+       if(inputValue === "I want to learn options trading") {
         console.log('matched')
           reply = {
             type: "button",
@@ -87,6 +108,66 @@ export default {
               {
                 text: "Click to learn about options",
                 value: "https://www.investopedia.com/terms/o/option.asp",
+                action: "url",
+              },
+            ]
+          };
+      }
+      if(inputValue === "How can I see my order status?") {
+        console.log('matched')
+          reply = {
+            type: "button",
+            text: "Go to https://www.investopedia.com/simulator/ and login using your credentials. Select your game from dropdown on left top corner. Click on Trade -> Order Status.",
+            agent: 'bot',
+            options: [
+              {
+                text: "Click to see your order status:",
+                value: "https://www.investopedia.com/simulator/trade/order-status",
+                action: "url",
+              },
+            ]
+          };
+      }
+      if(inputValue === "What is the current price of AAPL?") {
+        console.log('matched')
+          reply = {
+            type: "button",
+            text: "Market is closed now but last closing price for AAPL (Apple) was 186.86 USD",
+            agent: 'bot',
+            options: [
+              {
+                text: "Click to search stock symbols:",
+                value: "https://www.investopedia.com/simulator/trade/stocks",
+                action: "url",
+              },
+            ]
+          };
+        }
+          if(inputValue === "What are the popular stocks today?") {
+          console.log('matched')
+          reply = {
+            type: "button",
+            text: "Market is closed now but you can find Most Traded Stocks here: ",
+            agent: 'bot',
+            options: [
+              {
+                text: "Click to find Most Traded Stocks:",
+                value: "https://www.investopedia.com/simulator/research",
+                action: "url",
+              },
+            ]
+          };
+        }
+        if(inputValue === "Where can I trade real time?") {
+          console.log('matched')
+          reply = {
+            type: "button",
+            text: "Investopedia Simulator is a paper trading application that allows users to learn about investing and experiment using different strategies without risking real money. For real time trading, Go and vist our sponsered partner: ",
+            agent: 'bot',
+            options: [
+              {
+                text: "Click to do real time trading:",
+                value: "www.trading-sponser.com",
                 action: "url",
               },
             ]
@@ -105,6 +186,7 @@ export default {
     <VueBotUI
       :messages="this.messages"
       :bot-typing="this.botTyping"
+      :options="this.botOptions"
       @init="this.botStart"
       @msg-send="msgSend"
     />
